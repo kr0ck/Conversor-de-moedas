0f504861746e4052fd45952ced8416d6eca15d96
kr0ck <hebertcampos@hotmail.com.br>
2021-01-28 15:43:04 -0300

    HomeController esta linkado a HomePage

--- lib/app/Views/home_page.dart
+++ lib/app/Views/home_page.dart
@@ -1,8 +1,17 @@
+import 'package:conversor_de_moedas/app/Controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'Componentes/box_moeda_componente.dart';
 
 class HomePage extends StatelessWidget {
+  final TextEditingController deTexto = TextEditingController();
+  final TextEditingController paraTexto = TextEditingController();
+  HomeController homeController;
+
+  HomePage() {
+    homeController = new HomeController(deTexto: deTexto, paraTexto: paraTexto);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
