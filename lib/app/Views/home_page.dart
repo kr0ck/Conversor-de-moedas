77db5e97591df3f3508f371b6890600f6c476dde
kr0ck <hebertcampos@hotmail.com.br>
2021-01-28 11:12:36 -0300

    Definindo os widgets da tela

--- lib/app/Views/home_page.dart
+++ lib/app/Views/home_page.dart
@@ -4,10 +4,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset('assets/logo.png'),
-        ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Image.asset('assets/logo.png'),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(children: [
+                Expanded(
+                  flex: 1,
+                  child: DropdownButton(
+                    items: [],
+                    onChanged: (value) {},
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Valor',
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Converter'),
+            ),
+          ],
+        ),
       ),
     );
   }
