a19d090275e793068978abd7bc55334628efa90d
kr0ck <hebertcampos@hotmail.com.br>
2021-01-28 11:19:13 -0300

    Organizando o layaut

--- lib/app/Views/home_page.dart
+++ lib/app/Views/home_page.dart
@@ -7,34 +7,45 @@ class HomePage extends StatelessWidget {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            Image.asset('assets/logo.png'),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(children: [
-                Expanded(
-                  flex: 1,
-                  child: DropdownButton(
-                    items: [],
-                    onChanged: (value) {},
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Valor',
+        child: Padding(
+          padding:
+              const EdgeInsets.only(right: 20, left: 20, bottom: 10, top: 100),
+          child: Column(
+            children: [
+              Image.asset('assets/logo.png', width: 150, height: 150),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(children: [
+                  Expanded(
+                    flex: 1,
+                    child: DropdownButton(
+                      items: [],
+                      onChanged: (value) {},
                     ),
                   ),
-                ),
-              ]),
-            ),
-            RaisedButton(
-              onPressed: null,
-              child: Text('Converter'),
-            ),
-          ],
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          // labelText: 'Valor',
+                          ),
+                    ),
+                  ),
+                ]),
+              ),
+              SizedBox(height: 50),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('CONVERTER'),
+              ),
+            ],
+          ),
         ),
       ),
     );
