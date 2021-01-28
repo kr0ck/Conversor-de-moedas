1a66678461d141792dbbdd5378e187951fb784a0
kr0ck <hebertcampos@hotmail.com.br>
2021-01-28 11:59:24 -0300

    Colocando o singleScrowView para rolar a tela

--- lib/app/Views/home_page.dart
+++ lib/app/Views/home_page.dart
@@ -6,26 +6,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding:
-              const EdgeInsets.only(right: 20, left: 20, bottom: 10, top: 100),
-          child: Column(
-            children: [
-              Image.asset('assets/logo.png', width: 150, height: 150),
-              SizedBox(
-                height: 50,
-              ),
-              BoxModedaComponente(),
-              SizedBox(height: 50),
-              RaisedButton(
-                color: Colors.amber,
-                onPressed: () {},
-                child: Text('CONVERTER'),
-              ),
-            ],
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                right: 20, left: 20, bottom: 10, top: 100),
+            child: Column(
+              children: [
+                Image.asset('assets/logo.png', width: 150, height: 150),
+                SizedBox(
+                  height: 50,
+                ),
+                BoxModedaComponente(),
+                BoxModedaComponente(),
+                SizedBox(height: 50),
+                RaisedButton(
+                  color: Colors.amber,
+                  onPressed: () {},
+                  child: Text('CONVERTER'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
