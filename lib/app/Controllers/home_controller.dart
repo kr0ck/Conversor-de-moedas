ad162ed12ef0f42e4394bc461b94437c3c0f477f
kr0ck <hebertcampos@hotmail.com.br>
2021-01-28 12:08:02 -0300

    Instanciando aquivos que serão trabalhados

--- lib/app/Controllers/home_controller.dart
+++ lib/app/Controllers/home_controller.dart
@@ -1,1 +1,13 @@
-class HomeController {}
+import 'package:conversor_de_moedas/app/Models/moeda_model.dart';
+
+class HomeController {
+  List<MoedaModel> moedas;
+  MoedaModel deMoeda;
+  MoedaModel paraMoeda;
+
+  HomeController() {
+    moedas = MoedaModel.getMoedas();
+    deMoeda = moedas[0];
+    paraMoeda = moedas[1];
+  }
+}
