2a0b68b67c0bb5687445c764754a5f20fe16266d
kr0ck <hebertcampos@hotmail.com.br>
2021-01-28 11:39:27 -0300

    Retirando sizedBox da row

--- lib/app/Views/home_page.dart
+++ lib/app/Views/home_page.dart
@@ -16,46 +16,43 @@ class HomePage extends StatelessWidget {
               SizedBox(
                 height: 50,
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Row(children: [
-                  Expanded(
-                    flex: 1,
-                    child: SizedBox(
-                      height: 65,
-                      child: DropdownButton(
-                        iconEnabledColor: Colors.amber,
-                        isExpanded: true,
-                        underline: Container(
-                          height: 1,
-                          color: Colors.amber,
-                        ),
-                        items: [
-                          DropdownMenuItem(child: Text('Real')),
-                          DropdownMenuItem(child: Text('Dolar')),
-                        ],
-                        onChanged: (value) {},
+              Row(children: [
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                    height: 65,
+                    child: DropdownButton(
+                      iconEnabledColor: Colors.amber,
+                      isExpanded: true,
+                      underline: Container(
+                        height: 1,
+                        color: Colors.amber,
+                      ),
+                      items: [
+                        DropdownMenuItem(child: Text('Real')),
+                        DropdownMenuItem(child: Text('Dolar')),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amber),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amber),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.amber),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.amber),
-                        ),
-                      ),
-                    ),
-                  ),
-                ]),
-              ),
+                ),
+              ]),
               SizedBox(height: 50),
               RaisedButton(
                 color: Colors.amber,
